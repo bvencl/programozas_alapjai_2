3572fa4f6cd85061dfd5c5232d4e6fc0f0a603f8
Bodi.Vencel <150474053+bvencl@users.noreply.github.com>
2024-02-22 22:06:45 +0100

    ize

---
+++ labor/lab2/02_LAB_IMSC_HAL_KOD/qsort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+void asd_qsort(void *asd, int n, int size, int (*f)(const void *a, const void *b))
+{
+
+
+
+
+
+};
+
+int main ()
+{
+
+    qsort();
+
+
+    return 0;
+}

de73beba2188bc5f3b0c2c21cffcc44301683eb2
Bodi.Vencel <150474053+bvencl@users.noreply.github.com>
2024-02-25 15:07:15 +0100

    Add math.h and debugmalloc.h headers, implement own_qsort function

--- imsc/02_LAB_IMSC_HAL_KOD/qsort_main.c
+++ imsc/02_LAB_IMSC_HAL_KOD/qsort_main.c
@@ -1,5 +1,7 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include "debugmalloc.h"
 
 #define ARRAY_MAX 5
 
@@ -9,7 +11,6 @@
 // tömböt átadhatunk volna a qsortnak, és
 // ő nem tud a típusról.
 // Miért void pointerek a paraméterei?
-// Mert nem kell előre definiálni, hogy milyen típusú változó értéékét szeretnénk átadni neki
 
 int compare(const void *a, const void *b)
 {
@@ -23,39 +24,52 @@ int compare(const void *a, const void *b)
 		return 0;
 	else
 		return 1;
+	// Válaszok:
+		//igen, úgy működik ahogyan vártuk
+		//Mert így több különböző szempont szerint rendezhetjük sorba a tömbök elemeit
+		//"visszatérési érték" "(*fgvnév)("asd" pram1, "asd" param2...)",
+		// Mert így bármilyen típussal tud dolgozni az összehasonlító
 }
 
 // 5. FELADAT: qsort paraméterlistájával megegyező függvény, amely rendező algoritmust valósít meg
+void own_qsort(void *base, size_t nmemb, size_t size, int(*compar)(const void *, const void *))
+{
 
-
-
+}
 
 int main()
 {
 	// 2. FELADAT: tömbméret bekérés a felhasználótól. Használj dinamikus memóriakezelést!
 
 	int n;
-	scanf("%d", &n);
+	printf("Number of elemts in the array: "),
+		scanf_s("%d", &n, sizeof(int));
 
-	double *d = (double*)malloc(n * sizeof(double));
-
-	// double d[ARRAY_MAX];
-	int i;
+	double *d = (double *)malloc(n * sizeof(double));
 
 	// 4. FELADAT: biztonságos szövegbevitel. Tipp: scanf_s
 	// miért kell scanf_s: ha olyan területre írunk, amit nem foglaltunk le (akár csak kis részben is) => segmentation fault hiba
 	// hacker/óvatlan felhasználó leállíthatja a programunk: buffer overflow attack
 	// Warning kikapcsolás: C/C++ -> Preprocessor -> Preprocessor definitions: _CRT_SECURE_NO_WARNINGS
-	for (i = 0; i < ARRAY_MAX; i++)
+
+
+	printf("Let's fill the array!\n");
+
+	int i;
+	for (i = 0; i < n; i++)
 	{
 		printf("Enter a number:");
 		// 3. FELADAT: scanf háromféleképpen
-		scanf("%lf", &d[i]);
+		scanf_s("%lf", &d[i], sizeof(d[i]));
+		// scanf("%lf", &d[i]);
+		// fscanf(stdin, "%lf", &d[i]);
+
+		// azért pointert vár a scanf függvény, mert így tudja módosítani a változó értékét referencia funkció nélkül
 	}
 
-	qsort(d, ARRAY_MAX, sizeof(double), compare);
+	own_qsort(d, ARRAY_MAX, sizeof(double), compare);
 
-	for (i = 0; i < ARRAY_MAX; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%lf ", d[i]);
 	}
@@ -63,4 +77,6 @@ int main()
 	// 2. FELADAT: a korábban lefoglalt dinamikus területet ne felejtsd el felszabadítani!
 
 	free(d);
+
+	return 0;
 }

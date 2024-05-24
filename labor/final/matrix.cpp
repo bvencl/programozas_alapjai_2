e1d4e0925b9ce8466552ad500a0333f4ad8108ec
Bodi.Vencel <150474053+bvencl@users.noreply.github.com>
2024-05-24 17:53:29 +0200

    Final with matrix

--- labor/final/matrix.cpp
+++ labor/final/matrix.cpp
@@ -1,56 +1,107 @@
 #include <iostream>
-#include <ostream>
+#include <functional>
 
 template <class Derived>
 class MatrixBase
 {
-
 public:
-    unsigned rows() const
-    {
-        return static_cast<Derived const &>(*this).rows();
-    }
-    unsigned cols() const
-    {
-        return static_cast<Derived const &>(*this).cols();
-    }
+	static unsigned const ROWS = Derived::ROWS;
+	static unsigned const COLS = Derived::COLS;
 
-    double operator()(unsigned row, unsigned col)
-    {
-        return static_cast<Derived const &>(*this)(row, col);
-    }
+	Derived const &derived() const { return static_cast<Derived const &>(*this); }
+	Derived &derived() { return static_cast<Derived &>(*this); }
 
-    void print(std::ostream &os)
-    {
-        for (unsigned row = 0; row < rows(); row++)
-        {
-            for (unsigned col = 0; col < cols(); col++)
+	unsigned rows() const { return derived().rows(); }
+	unsigned cols() const { return derived().cols(); }
+	double operator()(unsigned row, unsigned col) const { return derived()(row, col); }
 
-                os << (*this)(row, col) << ' ';
-            os << std::endl;
-        }
-    }
+	void print(std::ostream &os = std::cout) const
+	{
+		for (unsigned row = 0; row < rows(); row++)
+		{
+			for (unsigned col = 0; col < cols(); col++)
+				os << (*this)(row, col) << ' ';
+			os << std::endl;
+		}
+	}
 };
 
 template <unsigned Rows, unsigned Cols>
 class Matrix : public MatrixBase<Matrix<Rows, Cols>>
 {
 public:
-    unsigned rows() const { return Rows; }
-    unsigned cols() const { return Cols; }
+	static unsigned const ROWS = Rows;
+	static unsigned const COLS = Cols;
+
+	unsigned rows() const { return ROWS; }
+	unsigned cols() const { return COLS; }
+	double operator()(unsigned row, unsigned col) const { return elements[row][col]; }
 
 private:
-    double elements[Rows][Cols];
-
-    double operator()(unsigned row, unsigned col)
-    {
-        return elements[row][col];
-    }
+	double elements[Rows][Cols];
 };
+
+template <unsigned Rows>
+class UnitMatrix : public MatrixBase<UnitMatrix<Rows>>
+{
+public:
+	static unsigned const ROWS = Rows;
+	static unsigned const COLS = Rows;
+
+	unsigned rows() const { return ROWS; }
+	unsigned cols() const { return COLS; }
+	double operator()(unsigned row, unsigned col) const { return row == col ? 1.0 : 0.0; }
+};
+
+template <class LhsDerived, class RhsDerived, class BinOp>
+class ElemwiseBinaryOp : public MatrixBase<ElemwiseBinaryOp<LhsDerived, RhsDerived, BinOp>>
+{
+	static_assert(LhsDerived::ROWS == RhsDerived::ROWS && LhsDerived::COLS == RhsDerived::COLS, "Elementwise binary operation on matrices of different size");
+
+public:
+	static unsigned const ROWS = LhsDerived::ROWS;
+	static unsigned const COLS = LhsDerived::COLS;
+
+	ElemwiseBinaryOp(LhsDerived const &lhs, RhsDerived const &rhs, BinOp op)
+		: lhs(lhs), rhs(rhs), op(op)
+	{
+	}
+
+	unsigned rows() const { return ROWS; }
+	unsigned cols() const { return COLS; }
+	double operator()(unsigned row, unsigned col) const { return op(lhs(row, col), rhs(row, col)); }
+
+private:
+	LhsDerived const &lhs;
+	RhsDerived const &rhs;
+	BinOp op; // functors are conventionally stored and passed by value
+};
+
+template <class LhsDerived, class RhsDerived>
+ElemwiseBinaryOp<LhsDerived, RhsDerived, std::plus<double>>
+operator+(MatrixBase<LhsDerived> const &lhs, MatrixBase<RhsDerived> const &rhs)
+{
+	return ElemwiseBinaryOp<LhsDerived, RhsDerived, std::plus<double>>(lhs.derived(), rhs.derived(), std::plus<double>());
+}
+
+template <class LhsDerived, class RhsDerived>
+ElemwiseBinaryOp<LhsDerived, RhsDerived, std::minus<double>>
+operator-(MatrixBase<LhsDerived> const &lhs, MatrixBase<RhsDerived> const &rhs)
+{
+	return ElemwiseBinaryOp<LhsDerived, RhsDerived, std::minus<double>>(lhs.derived(), rhs.derived(), std::minus<double>());
+}
 
 int main()
 {
-    Matrix<4, 4> m;
-    m.print(std::cout);
-    return 0;
+	UnitMatrix<3> u;
+	Matrix<3, 3> m1, m2;
+	(u + m1 - m2).print();
+
+#if 0
+	// HW: transposition with lazy evaluation
+	Matrix<4,5> m3, m4;
+	(m3 - m4).transposed().print();
+#endif
+	
+	return 0;
 }

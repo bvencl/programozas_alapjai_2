bc7fde68d35fe323003a16135472dcd27e3eb90a
Bodi.Vencel <150474053+bvencl@users.noreply.github.com>
2024-05-26 10:05:51 +0200

    szamolo aktualis

--- random/szamolo.m
+++ random/szamolo.m
@@ -1,5 +1,5 @@
-zh1 = (19/27) * 100;
-zh2  = (24/27) * 100;
+zh1 = (18.5/27) * 100;
+zh2  = (21.5/23) * 100;
 
 
 
@@ -7,7 +7,6 @@ hazi = 100;
 
 kiszhk = 90 + 90 + 80;
 
-vegeredmeny = zh1/3 + zh2/3 + hazi/6 + kiszhk/18;
+vegeredmeny = zh1/3 + zh2/3 + hazi/6 + kiszhk/18
 
-pzh = (23/27) * 100; % 81% minimum
-pzhval = 2*pzh/3 + hazi/6 + kiszhk/18
+csakzh2 = 2 * zh2/3 + hazi/6 + kiszhk/18
